f552cac6da203cb6f81b5d8367117933d6d38cc0
Alex Smith <Alex@Alexs-MacBook-Pro-7.local>
2015-05-11 22:52:21 +1000

    Final 11/5 commit. Data model set up. Linking data up to table views.

---
+++ CS193P Assignment 5 - Top Places/TopRegionsCDTVC.h
@@ -0,0 +1,15 @@
+//
+//  TopRegionsCDTVC.h
+//  CS193P Assignment 5 - Top Places
+//
+//  Created by Alex Smith on 11/05/2015.
+//  Copyright (c) 2015 Alex Smith. All rights reserved.
+//
+
+#import "CoreDataTableViewController.h"
+
+@interface TopRegionsCDTVC : CoreDataTableViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *context;
+
+@end
---
+++ CS193P Assignment 5 - Top Places/TopRegionsCDTVC.m
@@ -0,0 +1,32 @@
+//
+//  TopRegionsCDTVC.m
+//  CS193P Assignment 5 - Top Places
+//
+//  Created by Alex Smith on 11/05/2015.
+//  Copyright (c) 2015 Alex Smith. All rights reserved.
+//
+
+#import "TopRegionsCDTVC.h"
+
+@interface TopRegionsCDTVC ()
+
+@end
+
+@implementation TopRegionsCDTVC
+
+#pragma mark - Properties
+
+-(void)setContext:(NSManagedObjectContext *)context
+{
+    // set the fetched results controllers in here
+}
+
+#pragma mark - UITableViewDataSource
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    return nil;
+}
+
+@end
